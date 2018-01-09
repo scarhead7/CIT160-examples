0b60218880dd4f2d84ae74e4414fef17df8e3c4b
Dallin Snell <scarhead7@users.noreply.github.com>
2018-01-09 07:14:55 -0700

    Add files via upload
    * added conditional-ternary.c

---
+++ C/basics/conditional-ternary.c
@@ -0,0 +1,14 @@
+/* Snell, Dallin
+** 09 JAN 2018
+** A C code example: ternary conditional.
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  (void)printf("%s", (((1 + 1) == 2) ? "Math works!\n" : "Math is broken.\n"));
+
+  return 0;
+}
